ac5540041a2e760aa5f0ac344edb635806d28f50
Christos Marroquin <christos.marroquin@gmail.com>
2023-09-14 02:05:06 -0600

    carousel

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,27 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
 export default function HomePage() {
   return (
     <>
       <main>
         <div>DAPI</div>
+        <div className="col-6">
+          <Carousel
+            centerMode={true}
+            centerSlidePercentage="100"
+            dynamicHeight={false}
+            infiniteLoop={true}
+            showThumbs={false}
+          >
+            <div>
+              <img width={400} height={400} src="next-black.jpg" />
+            </div>
+            <div>
+              <img width={400} height={400} src="next-white.png" />
+            </div>
+          </Carousel>
+        </div>
       </main>
     </>
   );
